babf9ac22b154706ccb0f1bc449b49d934d854ed
jhun <jhun@nm-4ones.com>
2019-01-04 09:41:12 +0900

    case 추가

--- 1-1/jhun/java/src/test/java/IDFormCheckerTest.java
+++ 1-1/jhun/java/src/test/java/IDFormCheckerTest.java
@@ -27,18 +27,27 @@ public class IDFormCheckerTest {
     @Test
     public void firstNotEnglish() {
         String id = "1234";
+        String id2 = "!asdasd";
+        String id3 = "ㄱasdasd";
+
         Assert.assertFalse(FormChecker.check(CHECK_TYPE, id));
+        Assert.assertFalse(FormChecker.check(CHECK_TYPE, id2));
+        Assert.assertFalse(FormChecker.check(CHECK_TYPE, id3));
     }
 
     @Test
     public void firstEnglishWithKorean() {
         String id = "a하하하하";
+        String id2 = "a하asd";
         Assert.assertFalse(FormChecker.check(CHECK_TYPE, id));
+        Assert.assertFalse(FormChecker.check(CHECK_TYPE, id2));
     }
 
     @Test
     public void firstEnglishWithMark() {
         String id = "a@!#";
+        String id2 = "a123123.";
         Assert.assertFalse(FormChecker.check(CHECK_TYPE, id));
+        Assert.assertFalse(FormChecker.check(CHECK_TYPE, id2));
     }
 }
