283c821941e7d4d5dace84be3a898087cf0bbf68
jsh <jsh3658@naver.com>
2019-01-06 16:56:06 +0900

    'test'

--- 1-1/jang/spec/helloSpec.js
+++ 1-1/jang/spec/helloSpec.js
@@ -6,20 +6,17 @@ describe('id인 경우,', () => {
     });
 });
 
-
 describe('password인 경우,', () => {
     it('영어, 숫자, 특수문자를 포함 10개 이상', () => {
         expect(fromData().pass).toBe(true);
     });
 });
 
-
 describe('email인 경우,', () => {
     it('영어,숫자@영어,숫자.영어', () => {
         expect(fromData().email).toBe(true);
     });
 })
-
 
 describe('Phone인 경우,', () => {
     it('숫자(3)-숫자(3~4)-숫자(3~4)', () => {
