22074fb033a5058b985d13e511e01f7a59b6e9cd
jsh <jsh3658@naver.com>
2019-01-06 16:56:06 +0900

    'test'

--- 1-1/jang/app/userData.js
+++ 1-1/jang/app/userData.js
@@ -1,11 +1,5 @@
 module.exports = {
     dummy: [
-        {id: 'ybcdd_23efghijk424f24f$0si#$', pass: 'abc234234deff!@#$', email: 'jang1234@netmarble.net', phone: '010-1234-1245'},
-        {id: 'sd@#$%sdfg3@#$&%%&', pass: 'abc234234deff!@#$', email: 'jang1234@netmarble.net', phone: '010-1234-1245'},
-        {id: 'd123445sfg46746', pass: 'abc234234deff!@#$', email: 'jang1234@netmarble.net', phone: '010-1234-1245'},
-        {id: 'sljg@naver.com', pass: 'abc234234deff!@#$', email: 'jang1234@netmarble.net', phone: '010-1234-1245'},
-        {id: 'opera1245adsf', pass: 'abc234234deff!@#$', email: 'jang1234@netmarble.net', phone: '010-1234-1245'},
-        {id: 'aa@fd!fj9asdf4', pass: 'abc234234deff!@#$', email: 'jang1234@netmarble.net', phone: '010-1234-1245'},
-        {id: 'aaaaaafwef23fsa', pass: 'abc234234deff!@#$', email: 'jang1234@netmarble.net', phone: '010-1234-1245'}
+        {id: 'jsh3658322', pass: 'sdfas234d11A!#F##$!#g$', email: 'jang1234@netmarble.net', phone: '010-1334-1225'}
     ]
 }
