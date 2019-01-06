1936ebd7e0590167f9ee27983dcf6778b7d9e8a7
glqdlt <dlfdnd0725@gmail.com>
2019-01-06 17:08:55 +0900

    옳바른 이메일 추가

--- 1-1/jhun/java/src/test/java/EmailFormCheckerTest.java
+++ 1-1/jhun/java/src/test/java/EmailFormCheckerTest.java
@@ -10,7 +10,18 @@ public class EmailFormCheckerTest {
     public void simpleEmail() throws Exception {
         List<String> emails = Arrays.asList(
                 "jhun@jhun.com",
+                "jhun_jhun@jhun.com",
+                "jhun_2jhun@jhun.com",
+                "2jhun_2jhun@jhun.com",
+                "jhun_2jhun@jhun.com",
                 "jhun2@jhun.co.kr",
+                "jhun2@jhun.COM",
+                "jhun2@jhun.org",
+                "jhun2@jhun.ss.rr",
+                "jhun2@jhun.go.kr",
+                "jhun2@jhun2.go.kr",
+                "jhun2@jhu2_2.go.kr",
+                "jhun2@jhu2_com.go.kr",
                 "jhun2@jhun.net");
 
         Assert.assertFalse(emails.stream().anyMatch(x -> !FormChecker.check(CheckType.EMAIL, x)));
