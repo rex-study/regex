a1f0e1c2a1707e9062f07c122298e27f24d26517
jsh <jsh3658@naver.com>
2019-01-11 23:58:18 +0900

    'URL'

--- 1-1/jang/2주차/app/url.js
+++ 1-1/jang/2주차/app/url.js
@@ -1,32 +1,39 @@
 'use strict';
 
-let regex = /https?:\/\/www\.\w+\.([a-zA-Z]{2}\.)?[a-zA-Z]{2,3}(\/\w+)*(\?\w+\=\w+)*/gi;
+module.export = () => {
+    let regex = /https?:\/\/www\.\w+\.([a-zA-Z]{2}\.)?[a-zA-Z]{2,3}(\/\w+)*((\?\w+\=\w{1,2047})*\=$)?/gi;
+    let result = null;
 
-//올바른 URL
-const correctURL = [
-    'http://www.naver.co.kr',
-    'https://www.google.com/profile',
-    'http://www.jang6.co.kr/userEdit2_3',
-    'http://www.wwww.net/sub=',
-    'http://www.wwww.net/sub/bus/apple?id=23f23ff23ff2',
-    'http://www.wwww.net/sub?use_2r=asdfasldkflaskdf=',
-    'http://www.1111.net?id=jang?playerId=jgkj49jkslfks0328fjdkjsj=',
-    'http://www.cyworld.com/profile?usrName=jang='
-]
+    //올바른 URL
+    const correctURL = [
+        'http://www.naver.co.kr',
+        'https://www.google.com/profile',
+        'http://www.jang6.co.kr/userEdit2_3',
+        'http://www.wwww.net/sub=',
+        'http://www.wwww.net/sub/bus/apple?id=23f23ff23ff2=',
+        'http://www.wwww.net/sub?use_2r=asdfasldkflaskdf=',
+        'http://www.1111.net?id=jang?playerId=jgkj49jkslfks0328fjdkjsj=',
+        'http://www.cyworld.com/profile?usrName=jang='
+    ]
 
-//틀린 URL
-const incorrectURL = [
-    'ws://www.naver.com',
-    'http://www.google.com.net.kr.co.kr',
-    'http:www.google.com',
-    'http://wWW.@daum..net',
-    'https://www.yahoo.coo.krr',
-    'http://www.naver.com?id=',
-    'http://www.333333.com?1.1.1.',
-    'http://www.24323fsadfasdf.net?id=asdfasdf233244?product=fkafj230fkslkasdf30fksdfj=====',
-    'http://Www.11asdfasdfasdfas11.net?user=fsdf23f3?product=fkafj230=fkslkdf2+30fksdfj='
-]
+    //틀린 URL
+    const incorrectURL = [
+        'ws://www.naver.com',
+        'http://www.google.com.net.kr.co.kr',
+        'http:www.google.com',
+        'http://wWW.@daum..net',
+        'https://www.yahoo.coo.krr',
+        'http://www.naver.com?id=',
+        'http://www.333333.com?1.1.1.',
+        'http://www.24323fsadfasdf.net?id=asdfasdf233244?product=fkafj230fkslkasdf30fksdfj=====',
+        'http://www.11asdfasdfasdfas11.net?user=fsdf23f3?product=fkafj230fkslk+df230fksdfj='
+    ]
 
-correctURL.map(e => {
-    console.log(e.match(regex));
-});
+    correctURL.map(e => {
+        result = regex.test(e);
+    });
+
+    console.log(result);
+
+    return result;
+}
---
+++ 1-1/jang/2주차/spec/urlSpec.js
@@ -0,0 +1,7 @@
+let URLSpec = require('../app/url');
+
+describe('URL', () => {
+    it('올바른 URL', () => {
+        expect(URLSpec).toBe(true);
+    });
+});
